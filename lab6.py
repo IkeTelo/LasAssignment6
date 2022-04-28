0b956508150dffa527662d7bcaf19417bd9c3213
Isaac Telloian <iketelloian@fresnostate.edu>
2022-04-28 07:27:27 -0700

    initial commit

---
+++ lab6.py
@@ -0,0 +1,16 @@
+# Laymens Terms!!!
+"""
+To start, we will generate a random interger between 1 and 20; then
+based on the result of the random number, we check to see if it falls under
+a certin range
+if the number is > 15, then the result will = "Cherries"
+otherwise if the number is > 10, the result will = "Orange"
+otherwise if the number is > 5, the result will = "Plum"
+otherwise if the number is > 2, the result will = "Melon"
+otherwise if the number is > 1, the result will = "Bell"
+if the number is not any of the ubove, then the result will = "Bar"
+
+we iterate over using a loop three times and print the results to the user.
+An example "Plum Cherries Melon"
+
+"""
