c59dc55f6d0a2a24e8e4c75b5fb27d6f77bc2aec
Isaac Telloian <iketelloian@fresnostate.edu>
2022-04-28 07:31:04 -0700

    finished pseudocode

--- lab6.py
+++ lab6.py
@@ -14,3 +14,26 @@ we iterate over using a loop three times and print the results to the user.
 An example "Plum Cherries Melon"
 
 """
+# Psudocode !!!
+
+"""
+import random
+num = generate random number between 1 and 20
+
+if the number is > 15,
+    then the result will = "Cherries"
+otherwise if num is > 10,
+    the result will = "Orange"
+otherwise if num is > 5,
+    the result will = "Plum"
+otherwise if num is > 2,
+    the result will = "Melon"
+otherwise if num is > 1,
+    the result will = "Bell"
+otherwise
+    the result will be "Bar"
+
+loop three times
+    print the output (fruit) to the user
+
+"""
